bed642c60a43f33f859923310eee1685b0678b91
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-23 14:32:40 +0600

    login page basic setup completed

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,22 @@
 const LoginPage = () => {
   return (
-    <div>
+    <div className="mx-12">
       <h1>Hello, From LoginPage!</h1>
+      <form action="" className="flex flex-col gap-4 w-40">
+        <input
+          className="border bg-gray-50 p-2 rounded-md "
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          className="border bg-gray-50 p-2 rounded-md "
+          type="text"
+          placeholder="Password"
+        />
+        <button className="p-2 bg-custom-blue text-white rounded-md">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
