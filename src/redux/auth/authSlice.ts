c6c4f6dbd38f55d38fe093fca9eeb44bdd39fb14
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-24 00:59:54 +0600

    login mutation implemented

--- src/redux/auth/authSlice.ts
+++ src/redux/auth/authSlice.ts
@@ -13,5 +13,18 @@ const initialState: TAuthState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
 });
+
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth/authSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
