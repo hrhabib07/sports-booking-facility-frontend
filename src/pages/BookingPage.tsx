606c9e4e329a960b6fcc1822d20c549f28522ad0
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-24 21:49:19 +0600

    facility selection added

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,14 +1,39 @@
-import MyBookings from "../components/ui/MyBooking";
+import { Select, Space } from "antd";
+import { useGetAllFacilitiesQuery } from "../redux/facilities/facilitiesApi";
 
 const BookingPage = () => {
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+
+  const { data } = useGetAllFacilitiesQuery(undefined);
+
+  const facilitiesOptions = data?.data.map(
+    (item: { _id: string; name: string }) => ({
+      value: item._id,
+      label: item.name,
+    })
+  );
+  // console.log(facilitiesOptions);
+
+  console.log(data);
   return (
     <div>
-      <div>
-        <h2> my booking </h2>
-        <MyBookings></MyBookings>
-      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div>div1</div>
+        <div>
+          <Space className="">
+            <div className="flex flex-col">
+              <label htmlFor="facility">Select a facility</label>
+              <Select
+                id="facility"
+                defaultValue="select a facility "
+                className="w-80"
+                onChange={handleChange}
+                options={facilitiesOptions}
+              />
+            </div>
+          </Space>
+        </div>
         <div>dvi2</div>
       </div>
     </div>
---
+++ src/redux/facilities/facilitiesApi.ts
@@ -0,0 +1,22 @@
+import { baseApi } from "../api/baseApi";
+
+const facilitiesApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllFacilities: builder.query({
+      query: () => ({
+        url: "/facility",
+        method: "GET",
+        // body: userInfo,
+      }),
+    }),
+    // userSignUp: builder.mutation({
+    //   query: (userInfo) => ({
+    //     url: "/auth/signup",
+    //     method: "POST",
+    //     body: userInfo,
+    //   }),
+    // }),
+  }),
+});
+
+export const { useGetAllFacilitiesQuery } = facilitiesApi;
