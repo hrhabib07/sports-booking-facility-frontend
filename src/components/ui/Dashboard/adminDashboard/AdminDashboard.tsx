994d7ac10e26c6dd2847b9d6b4f454a26fd56cfa
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-11-12 09:17:23 +0600

    dashboard allignment updated

--- src/components/ui/Dashboard/adminDashboard/AdminDashboard.tsx
+++ src/components/ui/Dashboard/adminDashboard/AdminDashboard.tsx
@@ -33,103 +33,105 @@ const UserDashboard = () => {
   };
 
   return (
-    <Layout className="min-h-screen">
-      <Sider
-        width={250}
-        className="bg-white shadow-md hidden md:block"
-        breakpoint="md"
-        collapsedWidth="0"
-      >
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          className="h-full border-r-0"
+    <div className="max-w-7xl mx-auto">
+      <Layout className="min-h-screen">
+        <Sider
+          width={250}
+          className="bg-white shadow-md hidden md:block"
+          breakpoint="md"
+          collapsedWidth="0"
         >
-          <Menu.Item key="1">
-            <Link to="">
-              <span className="text-gray-600 hover:text-custom-blue">
-                All Facility
-              </span>
-            </Link>
-          </Menu.Item>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            className="h-full border-r-0"
+          >
+            <Menu.Item key="1">
+              <Link to="">
+                <span className="text-gray-600 hover:text-custom-blue">
+                  All Facility
+                </span>
+              </Link>
+            </Menu.Item>
 
-          <Menu.Item key="7">
-            <Link to="create-facility">
-              <span className="text-gray-600 hover:text-custom-blue">
-                Create Facility
-              </span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="all-booking">
-              <span className="text-gray-600 hover:text-custom-blue">
-                All Booking
-              </span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="8">
-            <Link to="create-admin">
-              <span className="text-gray-600 hover:text-custom-blue">
-                Create Admin
-              </span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="profile-info">
-              <span className="text-gray-600 hover:text-custom-blue">
-                Profile Info
-              </span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <button
-              onClick={showLogoutConfirm}
-              className="text-left text-gray-600 w-full hover:text-custom-blue"
-            >
-              Logout
-            </button>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Content className="p-2 md:p-6 bg-gray-50">
-          <div className="md:hidden mb-4">
-            <Menu mode="horizontal" className="w-full">
-              <Menu.Item key="4">
-                <Link to="">
-                  <span className="text-gray-600 hover:text-custom-blue">
-                    Dashboard
-                  </span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="1">
-                <Link to="all-booking">
-                  <span className="text-gray-600 hover:text-custom-blue">
-                    All Booking
-                  </span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Link to="profile-info">
-                  <span className="text-gray-600 hover:text-custom-blue">
-                    Profile Info
-                  </span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <button
-                  onClick={showLogoutConfirm}
-                  className="text-left text-gray-600 w-full hover:text-custom-blue"
-                >
-                  Logout
-                </button>
-              </Menu.Item>
-            </Menu>
-          </div>
-          <Outlet />
-        </Content>
+            <Menu.Item key="7">
+              <Link to="create-facility">
+                <span className="text-gray-600 hover:text-custom-blue">
+                  Create Facility
+                </span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Link to="all-booking">
+                <span className="text-gray-600 hover:text-custom-blue">
+                  All Booking
+                </span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="8">
+              <Link to="create-admin">
+                <span className="text-gray-600 hover:text-custom-blue">
+                  Create Admin
+                </span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="profile-info">
+                <span className="text-gray-600 hover:text-custom-blue">
+                  Profile Info
+                </span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <button
+                onClick={showLogoutConfirm}
+                className="text-left text-gray-600 w-full hover:text-custom-blue"
+              >
+                Logout
+              </button>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content className="p-2 md:p-6 bg-gray-50">
+            <div className="md:hidden mb-4">
+              <Menu mode="horizontal" className="w-full">
+                <Menu.Item key="4">
+                  <Link to="">
+                    <span className="text-gray-600 hover:text-custom-blue">
+                      Dashboard
+                    </span>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="1">
+                  <Link to="all-booking">
+                    <span className="text-gray-600 hover:text-custom-blue">
+                      All Booking
+                    </span>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <Link to="profile-info">
+                    <span className="text-gray-600 hover:text-custom-blue">
+                      Profile Info
+                    </span>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="3">
+                  <button
+                    onClick={showLogoutConfirm}
+                    className="text-left text-gray-600 w-full hover:text-custom-blue"
+                  >
+                    Logout
+                  </button>
+                </Menu.Item>
+              </Menu>
+            </div>
+            <Outlet />
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </div>
   );
 };
 
--- src/components/ui/Dashboard/userDashboard/UserDashboard.tsx
+++ src/components/ui/Dashboard/userDashboard/UserDashboard.tsx
@@ -32,89 +32,91 @@ const UserDashboard = () => {
   };
 
   return (
-    <Layout className="min-h-screen">
-      <Sider
-        width={250}
-        className="bg-white shadow-md hidden md:block"
-        breakpoint="md"
-        collapsedWidth="0"
-      >
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          className="h-full border-r-0"
+    <div className="max-w-7xl mx-auto">
+      <Layout className="min-h-screen">
+        <Sider
+          width={250}
+          className="bg-white shadow-md hidden md:block"
+          breakpoint="md"
+          collapsedWidth="0"
         >
-          <Menu.Item key="1">
-            <Link to="">
-              <span className="text-gray-600 hover:text-custom-blue">
-                Dashboard
-              </span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="my-booking">
-              <span className="text-gray-600 hover:text-custom-blue">
-                My Booking
-              </span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="profile-info">
-              <span className="text-gray-600 hover:text-custom-blue">
-                Profile Info
-              </span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <button
-              onClick={showLogoutConfirm}
-              className="text-left text-gray-600 w-full hover:text-custom-blue"
-            >
-              Logout
-            </button>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Content className="p-2 md:p-6 bg-gray-50">
-          <div className="md:hidden mb-4">
-            {/* This menu will appear only on small screens */}
-            <Menu mode="horizontal" className="w-full">
-              <Menu.Item key="4">
-                <Link to="">
-                  <span className="text-gray-600 hover:text-custom-blue">
-                    Dashboard
-                  </span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="1">
-                <Link to="my-booking">
-                  <span className="text-gray-600 hover:text-custom-blue">
-                    My Booking
-                  </span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Link to="profile-info">
-                  <span className="text-gray-600 hover:text-custom-blue">
-                    Profile Info
-                  </span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <button
-                  onClick={showLogoutConfirm}
-                  className="text-left text-gray-600 w-full hover:text-custom-blue"
-                >
-                  Logout
-                </button>
-              </Menu.Item>
-            </Menu>
-          </div>
-          <Outlet />
-        </Content>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            className="h-full border-r-0"
+          >
+            <Menu.Item key="1">
+              <Link to="">
+                <span className="text-gray-600 hover:text-custom-blue">
+                  Dashboard
+                </span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Link to="my-booking">
+                <span className="text-gray-600 hover:text-custom-blue">
+                  My Booking
+                </span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="profile-info">
+                <span className="text-gray-600 hover:text-custom-blue">
+                  Profile Info
+                </span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <button
+                onClick={showLogoutConfirm}
+                className="text-left text-gray-600 w-full hover:text-custom-blue"
+              >
+                Logout
+              </button>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content className="p-2 md:p-6 bg-gray-50">
+            <div className="md:hidden mb-4">
+              {/* This menu will appear only on small screens */}
+              <Menu mode="horizontal" className="w-full">
+                <Menu.Item key="4">
+                  <Link to="">
+                    <span className="text-gray-600 hover:text-custom-blue">
+                      Dashboard
+                    </span>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="1">
+                  <Link to="my-booking">
+                    <span className="text-gray-600 hover:text-custom-blue">
+                      My Booking
+                    </span>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <Link to="profile-info">
+                    <span className="text-gray-600 hover:text-custom-blue">
+                      Profile Info
+                    </span>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="3">
+                  <button
+                    onClick={showLogoutConfirm}
+                    className="text-left text-gray-600 w-full hover:text-custom-blue"
+                  >
+                    Logout
+                  </button>
+                </Menu.Item>
+              </Menu>
+            </div>
+            <Outlet />
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </div>
   );
 };
 
